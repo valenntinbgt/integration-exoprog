486fb8b4badd9ff84154bc10404110e0a4f20507
adrienlefebvre69@gmail.com <adrienlefebvre69@gmail.com>
2011-05-27 18:53:36 +0200

    TEST avec ordi1

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html> 
+<!-- test avec ordi 1 !!-->
 <html lang="fr"> 
 	<head> 
 		<meta charset="iso-8859-1" /> 
@@ -7,38 +8,14 @@
 	</head> 
 	
 	<body> 
-        <div id="header"> 
-            <a href="index.php"><div id="logo"></div></a> 
- 
-            <div id="menu">
-                <p><a href="#">Accueil</a><a href="#">Exercices</a><a href="#">Membres</a></p>
-            </div>
-            
-            <div id="connexion"> 
-                <form method="post" name="connexion" action="#"> 
-                    <a href="" id="input_submit" onclick="connexion.submit(); return false;">Connexion</a>
-                    <input type="text" value="Pseudonyme" name="pseudo" id="input_pseudo" onfocus="if(this.value=='Pseudonyme') {this.value = '';}" onblur="if(this.value == ''){this.value='Pseudonyme';}"  size="15"/> 
-                    <input type="password" name="mdp" id="input_password" value="Mot de passe" size="15"  onfocus="if(this.value=='Mot de passe') {this.value = '';}" onblur="if(this.value == ''){this.value='Mot de passe';}"/> 
-                    <a href="inscription.php" id="input_inscription">Inscription</a>
-                </form> 
-            </div> 
-        </div>
+        <?php include('includes/header.php');?>
         
-        <div id="menu_droite"> 
-            <h5>Langages</h5> 
-            <ul> 
-                <li><a href="#">HTML/CSS</a></li>
-                <li><a href="#">JavaScript</a></li> 
-                <li><a href="#">PHP/MySQL</a></li> 
-                <li><a href="#">Langage C</a></li> 
-                <li><a href="#">Langage C++</a></li> 
-                <li><a href="#">Java</a></li> 
-                <li><a href="#">Python</a></li> 
-                <li><a href="#">VB .NET</a></li> 
-            </ul> 
-        </div> 
+        <p style="font-size: 0.7em" id="barre_nav">><a href="index.php">ExoProg</a>>Accueil</p>
+        
+        <?php include('includes/menu_droite.php');?>
         
         <div id="contenu"> 
+            
             <p>Bienvenue sur ExoProg, le site où vous trouverez nombre d'exercices de programmation.</p>
             <!-- Ici doit se trouver le contenu. -->
             <table id="accueil" width="100%">
@@ -103,9 +80,6 @@
             </table>    
         </div> 
         
-        <div id="footer">
-            <div id="footer_top"></div>
-            <p style="color: White">Footer !</p>
-        </div>	
+<?php include('includes/footer.php');?>
     </body> 
 </html> 
